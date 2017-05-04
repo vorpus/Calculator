d1fc380a324595bb018353dba4d25bbc2c5a05c3
lzhang <li3zhang@gmail.com>
2017-05-04 17:33:55 -0400

    init

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -9,17 +9,12 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    
+    @IBOutlet weak var Display: UILabel!
+    
+    @IBAction func Button(_ sender: UIButton) {
+        Display.text = Display.text! + sender.currentTitle!
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
 
 }
 
