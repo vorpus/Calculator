d2d5ab776ba3a7f2d576ea54eff075675db9fd8a
lzhang <li3zhang@gmail.com>
2017-05-04 18:20:27 -0400

    Add pi

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -12,8 +12,27 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var Display: UILabel!
     
+    var isUserTyping = false
+    
     @IBAction func Button(_ sender: UIButton) {
-        Display.text = Display.text! + sender.currentTitle!
+        if (isUserTyping) {
+            Display.text = Display.text! + sender.currentTitle!
+        } else {
+            Display.text = sender.currentTitle!
+            isUserTyping = true
+        }
+    }
+    
+    @IBAction func ActionButton(_ sender: UIButton) {
+        isUserTyping = false
+        switch (sender.currentTitle!) {
+        case "π":
+            Display.text = "3.141592"
+        case "√":
+            Display.text = "1"
+        default:
+            print("hi")
+        }
     }
 
 }
